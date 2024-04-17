adfb0aeb093716793804b596c6ef78fe1b2fc947
Gabriel-V-Mendes <147349102+Gabriel-V-Mendes@users.noreply.github.com>
2024-04-17 11:50:45 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,60 +4,38 @@
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catálogo de filmes</title>
+    <title>Tabuada</title>
 </head>
 <body>
 
-<h1>Escolhe um filme</h1>
+<header>
+<h1>Gerar Tabuada</h1>
+</header>
 
-<form action="#" method="post">
+<form action="" method="post">
 
-<select name="filme" id="#">
-    <option value="avatar">Avatar</option>
-    <option value="titanic">Titanic</option>
-    <option value="inception">A Origem</option>
-</select>
+<label for="number">Digite Um Numero</label>
 
-<input type="submit" value="Ver detalhes">
+<input type="number" name="number" id="number" required>
+
+<input type="submit" value="gerar tabuada" class="a">
 
 </form>
 
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
+if($_SERVER ["REQUEST_METHOD"] == "POST"){
 
-  switch($_POST["filme"]){
-    case "avatar":
-        $titulo = "Avatar";
-        $sinopse = "Avatar é um filme de ficção científica dirigido por James Cameron. Ambientado no ano de 2154, o filme segue a história de um ex-fuzileiro naval paraplégico que é enviado a Pandora em uma missão única, tornando-se parte do programa Avatar.";
-        $imagem = "https://m.media-amazon.com/images/I/91N1lG+LBIS._AC_UF894,1000_QL80_.jpg";
-        break;
-
-        case "titanic":
-            $titulo = "Titanic";
-            $sinopse = "Titanic é um filme de drama e romance dirigido por James Cameron, que retrata a história do naufrágio do RMS Titanic e o romance fictício entre dois passageiros durante sua viagem inaugural em 1912.";
-            $imagem = "https://upload.wikimedia.org/wikipedia/pt/2/22/Titanic_poster.jpg";
-            break;
-             case "inception":
-                $titulo = "A origem";
-                $sinopse = "A Origem é um filme de ficção científica dirigido por Christopher Nolan. Ele conta a história de Dom Cobb, um ladrão habilidoso que rouba segredos do subconsciente das pessoas enquanto estão sonhando.";
-                $imagem = "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg";
-                break;
-                default:
-                $titulo = "";
-                $sinopse = ""; 
-                $imagem = "";
-
-  }
-
-   if($titulo && $sinopse && $imagem){
-    echo "<h2>$titulo</h2>";
-    echo "<p> $sinopse <p>";
-    echo "<img src='$imagem' alt='$titulo'>";
-   } else{
-    echo "<p>Por favor selecione um filme";
-   }
-
+    $number = $_POST["number"];
+    $i = 1;
+    echo "<h3>Tabuada de $number</h3>";
+    echo "<ul>";
+    while($i <= 10){
+        $resultado = $number * $i;
+        echo "<li>$number x $i = $resultado</li>";
+        $i++;
+    }
+    echo "</ul>";
 }
 
 ?>
